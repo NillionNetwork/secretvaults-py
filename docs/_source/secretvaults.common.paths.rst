460bf8a9095fc00028e7c4684d5883708ef6a763
Andrei Lapets <a@lapets.io>
2025-09-15 03:24:31 -0400

    docs: ensure path constants appear in auto-generated docs

--- docs/_source/secretvaults.common.paths.rst
+++ docs/_source/secretvaults.common.paths.rst
@@ -5,3 +5,7 @@ common.paths
    :members:
    :undoc-members:
    :show-inheritance:
+
+.. autoclass:: secretvaults.common.paths.NilDbEndpointClass.v1
+   :members:
+   :undoc-members:
--- src/secretvaults/common/paths.py
+++ src/secretvaults/common/paths.py
@@ -1,76 +1,76 @@
 """
 API endpoint paths for Nildb client, matching the TypeScript NilDbEndpoint structure.
 """
+# pylint: disable=invalid-name,disable=too-few-public-methods
 
-
-class NilDbEndpoint:  # pylint: disable=too-few-public-methods
+class NilDbEndpoint: 
     """Root of all API endpoint paths for the Nildb client, matching the TypeScript NilDbEndpoint structure."""
 
-    class v1:  # pylint: disable=invalid-name,too-few-public-methods
+    class v1:
         """Version 1 API endpoints."""
 
-        class builders:  # pylint: disable=invalid-name,too-few-public-methods
+        class builders:
             """Endpoints for builder operations."""
 
-            register = "/v1/builders/register"
-            me = "/v1/builders/me"
+            register = '/v1/builders/register'
+            me = '/v1/builders/me'
 
-        class data:  # pylint: disable=invalid-name,too-few-public-methods
+        class data:
             """Endpoints for data operations."""
 
-            root = "/v1/data"
-            find = "/v1/data/find"
-            update = "/v1/data/update"
-            delete = "/v1/data/delete"
-            flushById = "/v1/data/:id/flush"
-            tailById = "/v1/data/:id/tail"
-            createOwned = "/v1/data/owned"
-            createStandard = "/v1/data/standard"
+            root = '/v1/data'
+            find = '/v1/data/find'
+            update = '/v1/data/update'
+            delete = '/v1/data/delete'
+            flushById = '/v1/data/:id/flush'
+            tailById = '/v1/data/:id/tail'
+            createOwned = '/v1/data/owned'
+            createStandard = '/v1/data/standard'
 
-        class queries:  # pylint: disable=invalid-name,too-few-public-methods
+        class queries:
             """Endpoints for query operations."""
 
-            root = "/v1/queries"
-            byId = "/v1/queries/:id"
-            run = "/v1/queries/run"
-            runById = "/v1/queries/run/:id"
+            root = '/v1/queries'
+            byId = '/v1/queries/:id'
+            run = '/v1/queries/run'
+            runById = '/v1/queries/run/:id'
 
-        class collections:  # pylint: disable=invalid-name,too-few-public-methods
+        class collections:
             """Endpoints for collection operations."""
 
-            root = "/v1/collections"
-            byId = "/v1/collections/:id"
-            indexesById = "/v1/collections/:id/indexes"
-            indexesByNameById = "/v1/collections/:id/indexes/:name"
+            root = '/v1/collections'
+            byId = '/v1/collections/:id'
+            indexesById = '/v1/collections/:id/indexes'
+            indexesByNameById = '/v1/collections/:id/indexes/:name'
 
-        class system:  # pylint: disable=invalid-name,too-few-public-methods
+        class system:
             """Endpoints for system operations and health checks."""
 
-            about = "/about"
-            health = "/health"
-            metrics = "/metrics"
-            openApiJson = "/openapi.json"
-            maintenanceStart = "/v1/system/maintenance/start"
-            maintenanceStop = "/v1/system/maintenance/stop"
-            logLevel = "/v1/system/log-level"
+            about = '/about'
+            health = '/health'
+            metrics = '/metrics'
+            openApiJson = '/openapi.json'
+            maintenanceStart = '/v1/system/maintenance/start'
+            maintenanceStop = '/v1/system/maintenance/stop'
+            logLevel = '/v1/system/log-level'
 
-        class users:  # pylint: disable=invalid-name,too-few-public-methods
+        class users:
             """Endpoints for user operations."""
 
-            me = "/v1/users/me"
+            me = '/v1/users/me'
 
-            class data:  # pylint: disable=invalid-name,too-few-public-methods
+            class data:
                 """Endpoints for user data operations."""
 
-                root = "/v1/users/data"
-                byId = "/v1/users/data/:collection/:document"
-                aclById = "/v1/users/data/:collection/:document/acl"
+                root = '/v1/users/data'
+                byId = '/v1/users/data/:collection/:document'
+                aclById = '/v1/users/data/:collection/:document/acl'
 
-                class acl:  # pylint: disable=invalid-name,too-few-public-methods
+                class acl:
                     """Endpoints for user data ACL operations."""
 
-                    grant = "/v1/users/data/acl/grant"
-                    revoke = "/v1/users/data/acl/revoke"
+                    grant = '/v1/users/data/acl/grant'
+                    revoke = '/v1/users/data/acl/revoke'
 
-
+NilDbEndpointClass = NilDbEndpoint
 NilDbEndpoint = NilDbEndpoint()
